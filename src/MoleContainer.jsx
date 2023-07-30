b68830a46ee0dda3d8dbcfb0dd119ed2cc134274
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-07-30 01:34:20 -0700

    Activity 3

---
+++ src/MoleContainer.jsx
@@ -0,0 +1,22 @@
+import Mole from './Mole'
+import EmptySlot from './EmptySlot'
+import { useState } from 'react'
+
+const MoleContainer = (props) => {
+    let [mole, setMole] = useState(false);
+
+    const handleClick = (e) => {
+      props.setScore(props.score + 1);
+      setMole(false);
+    }
+
+    let displayMole = mole ? <Mole setScore={props.setScore} toggle={setMole} handleClick={handleClick} /> : <EmptySlot toggle={setMole} />
+
+    return (
+      <div style={{'display': 'inline-block', 'width': '50vw'}}>
+        {displayMole}
+      </div>
+    );
+  }
+
+  export default MoleContainer;
